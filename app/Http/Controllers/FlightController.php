3cb003f89dbfe42dab88c9729192b03057ea5ee3
hardilpreet <hardilpreet.singh@workland.com>
2021-05-03 00:04:19 -0400

    set trip and flight class

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Helpers\response;
+use App\Helpers\flight;
+use App\Helpers\trip;
+use DateTime;
+use Validator;
 
 class FlightController extends Controller
 {
@@ -14,7 +17,7 @@ class FlightController extends Controller
     /**
         * @OA\Get(
         * path="/api/trips",
-        *  summary="Get Trips",
+        *  summary="Get All Trips",
         *  @OA\Parameter(name="from",
         *    in="query",
         *    description="Depature Airport Code",
@@ -45,6 +48,11 @@ class FlightController extends Controller
         *    description="Airline Code",
         *    @OA\Schema(type="string")
         *  ),
+         *  @OA\Parameter(name="stops",
+        *    in="query",
+        *    description="Number of stops",
+        *    @OA\Schema(type="integer")
+        *  ),
         *  @OA\Response(response="200",
         *    description="Validation Response",
         *  )
@@ -52,20 +60,32 @@ class FlightController extends Controller
     */
     public function getTrips(request $request)
     {
+
+        //add validation rules
+        $validator = Validator::make($request->all(), [
+            'from'=>'required|string',
+            'to'=>'required|string',
+            'departDate'=>'required|date_format:Y-m-d|after:today',
+            'returnDate'=>'nullable|date_format:Y-m-d|after_or_equal:departDate',
+            'stops'=>'nullable|integer',
+            'oneway'=>'nullable|string',
+            'airline'=>'nullable|string',
+        ]);
+        
+        if($validator->fails()){
+            return response()->json($validator->errors());
+        }
+
+        //get data from request
         $data = $request->all();
 
         $from = $data['from'] ?? false;
         $to = $data['to'] ?? false;
         $departDate = $data['departDate'] ?? false;
         $returnDate = $data['returnDate'] ?? false;
-        $stops = $data['stops'] ?? 0;
+        $stops = $data['stops'] ?? null;
         $oneway = (isset($data['oneway']) && $data['oneway'] == 'true') ?  true : false;
         $airline = isset($data['airline']) ?  $data['airline'] : false;
-
-        //add form validation
-        if(!$from || !$to || !$departDate || !$returnDate){
-            return 'Missing required input fields';
-        }
 
         $results = json_decode(file_get_contents(base_path('resources/sampleData.json')));
         $flights = $results->flights;
@@ -93,7 +113,7 @@ class FlightController extends Controller
         *  summary="Get All Countries",
         *  @OA\Parameter(name="code",
         *    in="query",
-        *    description="Airline Code",
+        *    description="Country Code",
         *    @OA\Schema(type="string")
         *  ),
         *  @OA\Response(response="200",
@@ -103,6 +123,13 @@ class FlightController extends Controller
     */
     public function getCountires(request $request)
     {
+        //add validation rules
+        $validator = Validator::make($request->all(), [
+            'code'=>'required|string',
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors());
+        }
 
         $data = $request->all();
         $code = isset($data['code']) ?  $data['code'] : false;
@@ -112,11 +139,11 @@ class FlightController extends Controller
 
         if($code){
             $data = array_filter($data,function($item) use($code){
-                return $item->code == $code;
+                return strtoupper($item->code) == strtoupper($code);
             });
         }
 
-        return response()->json($data);
+        return response()->json(['code'=>200, 'status'=> 'Sucess', 'data'=>$data]);
     }
 
     /**
@@ -125,7 +152,7 @@ class FlightController extends Controller
         *  summary="Get All Airports",
         *  @OA\Parameter(name="code",
         *    in="query",
-        *    description="Arrival Airport Code",
+        *    description="Airport Code",
         *    @OA\Schema(type="string")
         *  ),
         *  @OA\Response(response="200",
@@ -135,6 +162,14 @@ class FlightController extends Controller
     */
     public function getAirports(request $request)
     {
+        //add validation rules
+        $validator = Validator::make($request->all(), [
+            'code'=>'required|string',
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors());
+        }
+
         $data = $request->all();
         $code = isset($data['code']) ?  $data['code'] : false;
 
@@ -143,14 +178,15 @@ class FlightController extends Controller
 
         if($code){
             $data = array_filter($data,function($item) use($code){
-                return $item->code == $code;
+                return strtoupper($item->code) == strtoupper($code);
             });
         }
 
-        return response()->json($data);
+        return response()->json(['code'=>200, 'status'=> 'Sucess', 'data'=>$data]);
     }
 
 
+    //private methods for the class
     private function getFlights($flights, $to, $from, $departDate, $returnDate, $oneway = false, $stops = 0)
     {
         $data = []; 
@@ -167,9 +203,11 @@ class FlightController extends Controller
             if(strtoupper($flight->departure_airport) == strtoupper($from) && strtoupper($flight->arrival_airport) == strtoupper($to)){
 
                 $depatureTrips[] = $flight;
+
             }else{
 
                 $this->connections = [];
+                $this->stops = 1;
 
                 $response = $this->getConnections($flights, $from, $flight, $stops);
                 
@@ -177,34 +215,39 @@ class FlightController extends Controller
 
                     $depatureTrips[] = array_reverse($this->connections);
                 }
-
-                
             }
         }
 
         // return if it is oneway trip
         if($oneway){
+
             foreach($depatureTrips as $depature){
             
+                $stops = 0;
                 if(is_array($depature)){
                     $stops = count($depature)-1;
                     $fullPrice = 0;
                     foreach ($depature as $depart) {
+
                         $fullPrice += $depart->price;
                     }
                 }else{
-                    $stops = 0;
                     $fullPrice = $depature->price;
                 }
 
-                $trip = new \stdClass();
-                $trip->departDate = $departDate;
-                $trip->departFlight = $depature;
-                $trip->returnDate = null;
-                $trip->returnFlight = null;
-                $trip->fullPrice = number_format($fullPrice, 2, '.', '');
-                $trip->stops = $stops;
-                $trip->oneway = true;
+                // set depature flight Flight
+                $depatureFlight = new flight();
+                $depatureFlight->set_date($departDate);
+                $depatureFlight->set_flights($depature);
+                $depatureFlight->set_connections($stops);
+                $depatureFlight->set_price(number_format($fullPrice, 2, '.', ''));
+
+                // setup whole trip
+                $trip = new trip();
+                $trip->set_departure($depatureFlight);
+                $trip->set_fullPrice(number_format($depatureFlight->get_price(), 2, '.', ''));
+                $trip->set_oneway(true);
+
                 $data[] = $trip; 
             }
             return $data;
@@ -217,31 +260,48 @@ class FlightController extends Controller
         // var_dump("returnTrips",$returnTrips);die();
 
         foreach($depatureTrips as $depature){
+
             foreach($returnTrips as $return){
 
+                $stops = 0;
                 if(is_array($depature)){
+
                     $stops = count($depature)-1;
                     $fullPrice = 0;
                     foreach ($depature as $depart) {
+
                         $fullPrice += $depart->price;
                     }
                 }else{
-                    $stops = 0;
                     $fullPrice = $depature->price;
                 }
 
-                $trip = new \stdClass();
-                $trip->departDate = $departDate;
-                $trip->departFlight = $depature;
-                $trip->returnDate = $returnDate;
-                $trip->returnFlight = $return;
-                $trip->fullPrice = $fullPrice + $return->price;
-                $trip->oneway = false;
-                $trip->stops = $stops;
+                // set depature flight Flight
+                $depatureFlight = new flight();
+                $depatureFlight->set_date($departDate);
+                $depatureFlight->set_flights($depature);
+                $depatureFlight->set_connections($stops);
+                $depatureFlight->set_price(number_format($fullPrice, 2, '.', ''));
+
+                // set return flight Flight
+                $returnFlight = new flight();
+                $returnFlight->set_date($returnDate);
+                $returnFlight->set_flights($return);
+                $returnFlight->set_connections(0);
+                $returnFlight->set_price($return->price);
+                
+                // setup whole trip
+                $trip = new trip();
+                $trip->set_departure($depatureFlight);
+                $trip->set_return($returnFlight);
+                $trip->set_fullPrice($depatureFlight->get_price() + $returnFlight->get_price());
+                $trip->set_oneway(false);
+                
                 $data[] = $trip;
             }
 
         }
+
         return $data;
     }
 
@@ -255,6 +315,13 @@ class FlightController extends Controller
 
         if(!$landingFlights){
             $this->connections= []; 
+            $this->stops = 0;
+        }
+
+        if(isset($stops) && $this->stops > $stops){
+            
+            $this->connections= []; 
+            return;
         }
         
         foreach ($landingFlights as $key => $value) {
@@ -264,6 +331,7 @@ class FlightController extends Controller
                 $this->connections[] = $value;
                 return;
             }else{
+                $this->stops += 1;
                 $this->getConnections($flights, $from, $value, $stops );
             }
         }
